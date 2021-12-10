e6b3bb88e7fcfb55adfb7025c6107bf63f2f5924
SuketuPatni <suketublackpanther9@gmail.com>
2021-12-10 21:28:56 +0530

    rustfmt formatted and clippy linted

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 // // Inspired by https://www.nayuki.io/res/fast-fibonacci-algorithms/fastfibonacci.py
 
 // use std::io;
-use rug::{Integer};
+use rug::Integer;
 // use std::time::Instant;
 
-
-fn fibonacci(n:u64) -> (Integer, Integer) {
+fn fibonacci(n: u64) -> (Integer, Integer) {
     if n == 0 {
         (Integer::new(), Integer::from(1))
     } else {
-        let temp = fibonacci(n/2);
+        let temp = fibonacci(n / 2);
         let a = temp.0;
         let b = temp.1;
-        let c = &a * Integer::from(&b  * 2 - &a);
+        let c = &a * Integer::from(&b * 2 - &a);
         let d = Integer::from(&a * &a) + Integer::from(&b * &b);
         let e = Integer::from(&c + &d);
         if n % 2 == 0 {
-            (c,d)
+            (c, d)
         } else {
-            (d,e)
+            (d, e)
         }
     }
 }
